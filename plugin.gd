b6dfd66fce09ef66da1bd544ec490088d077a7e2
ZhangXiChang <yxrbeigua@outlook.com>
2024-08-30 14:24:17 +0800

    基础版本Spine导入器

--- plugin.gd
+++ plugin.gd
@@ -15,23 +15,5 @@ func _enter_tree() -> void:
 	window.hide()
 	add_tool_menu_item("Spine导入器",window.show)
 
-	test()
-
 func _exit_tree() -> void:
 	remove_tool_menu_item("Spine导入器")
-
-func test() -> void:
-	var spine_importer=SpineImporter.new()
-	if spine_importer.load(
-		"C:/Users/yxrbe/Project/Godot/demo/test/char_179_cgbird_sightseer_1.atlas",
-		"C:/Users/yxrbe/Project/Godot/demo/test/char_179_cgbird_sightseer_1.skel",
-		"C:/Users/yxrbe/Project/Godot/demo/test/char_179_cgbird_sightseer_1.png"
-	)!=OK:
-		push_error("加载文件失败")
-	else:
-		var scene=PackedScene.new()
-		if scene.pack(spine_importer.to_node2d("Spine"))!=OK:
-			push_error("保存为场景失败")
-		else:
-			ResourceSaver.save(scene,"res://test/test.tscn")
-	spine_importer.free()
--- res/spine-importer/spine_importer.gd
+++ res/spine-importer/spine_importer.gd
@@ -9,7 +9,6 @@ func _enter_tree() -> void:
 	editor_file_dialog.transient=true
 	editor_file_dialog.transient_to_focused=true
 	editor_file_dialog.file_selected.connect(file_selected)
-	editor_file_dialog.dir_selected.connect(dir_selected)
 	get_tree().root.add_child(editor_file_dialog)
 
 func _on_browse_atlas_file_pressed() -> void:
@@ -22,16 +21,34 @@ func _on_browse_skel_file_pressed() -> void:
 	editor_file_dialog.filters=["*.skel;骨架"]
 	editor_file_dialog.popup_file_dialog()
 
+func _on_browse_texture_file_pressed() -> void:
+	editor_file_dialog.file_mode=EditorFileDialog.FILE_MODE_OPEN_FILE
+	editor_file_dialog.filters=["*.png;纹理"]
+	editor_file_dialog.popup_file_dialog()
+
+func _on_browse_imput_path_pressed() -> void:
+	editor_file_dialog.file_mode=EditorFileDialog.FILE_MODE_SAVE_FILE
+	editor_file_dialog.filters=["*.tscn;场景"]
+	editor_file_dialog.popup_file_dialog()
+
 func file_selected(path:String)->void:
 	if path.get_extension()=="atlas":$VBoxContainer/HBoxContainer/AtlasFilePathInput.text=path
 	elif path.get_extension()=="skel":$VBoxContainer/HBoxContainer2/SkelFilePathInput.text=path
-
-func _on_browse_imput_path_pressed() -> void:
-	editor_file_dialog.file_mode=EditorFileDialog.FILE_MODE_OPEN_DIR
-	editor_file_dialog.popup_file_dialog()
-
-func dir_selected(path: String)->void:
-	$VBoxContainer/HBoxContainer3/ImputPath.text=path
+	elif path.get_extension()=="png":$VBoxContainer/HBoxContainer4/TextureFilePathInput.text=path
+	elif path.get_extension()=="tscn":$VBoxContainer/HBoxContainer3/ImputPathInput.text=path
 
 func _on_imput_spine_pressed() -> void:
-	pass
+	var spine_importer=SpineImporter.new()
+	if spine_importer.load(
+		$VBoxContainer/HBoxContainer/AtlasFilePathInput.text,
+		$VBoxContainer/HBoxContainer2/SkelFilePathInput.text,
+		$VBoxContainer/HBoxContainer4/TextureFilePathInput.text
+	)!=OK:
+		push_error("加载文件失败")
+	else:
+		var spine_scene=PackedScene.new()
+		if spine_scene.pack(spine_importer.to_node2d("Spine"))!=OK:
+			push_error("保存为场景失败")
+		else:
+			ResourceSaver.save(spine_scene,$VBoxContainer/HBoxContainer3/ImputPathInput.text)
+	spine_importer.free()
