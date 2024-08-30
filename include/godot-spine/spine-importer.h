48e2997afe2a6fc1efb5363c97a8d4f8daa08d6e
ZhangXiChang <yxrbeigua@outlook.com>
2024-08-30 18:49:38 +0800

    替换

--- include/godot-spine/spine-importer.h
+++ include/godot-spine/spine-importer.h
@@ -88,7 +88,7 @@ public:
                 skin->add_child(polygon);
                 polygon->set_owner(spine_node);
                 polygon->set_texture(this->texture);
-                polygon->set_name("Polygon");
+                polygon->set_name(slot->getData().getName().buffer());
                 polygon->set_skeleton(polygon->get_path_to(skeleton));
                 auto uv = PackedVector2Array();
                 for (size_t i = 0; i < mesh->getUVs().size(); i += 2)
